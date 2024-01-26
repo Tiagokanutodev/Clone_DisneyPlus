4aebd932c3533fae542ab44a4ef48fe182d02340
tiago canuto <tiagocanutogames@gmail.com>
2024-01-26 17:25:57 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,21 +1,75 @@
 document.addEventListener('DOMContentLoaded', function()
 {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
 
+
+
+    window.addEventListener('scroll', function()
+    {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero)
+        {
+            ocultaElementosDoHeader();
+        }
+        else
+        {
+            exibeElementosDoHeader();
+        }
+    })
+
+    //Seçao de atraçoes, programaçao das abas
     for (var i = 0; i < buttons.length; i++)
     {
         buttons[i].addEventListener('click', function(botao)
         {
             const abaAlvo = botao.target.dataset.tabButton; 
-        const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
             escondeTodasAbas();
             aba.classList.add('shows__list--is-active');
             removeBotaoAtivo();
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    //Seçao FAQ, accordion
+    for (var i = 0; i < questions.length; i++)
+    {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
+
+
+
+
+
+function ocultaElementosDoHeader()
+{
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+
+
+function exibeElementosDoHeader()
+{
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+
+
+
+function abreOuFechaResposta(elemento)
+{
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 
 
